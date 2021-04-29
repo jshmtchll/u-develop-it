247b15e397de78a07d4d0009764da43ab5402bfc
josh mitchell <jshmtchll@gmail.com>
2021-04-29 11:58:23 -0500

    merge

--- db/schema.sql
+++ db/schema.sql
@@ -17,10 +17,3 @@ CREATE TABLE candidates (
 );
 
 
-SELECT candidates.*, parties.name
-FROM candidates
-LEFT JOIN parties ON candidates.party_id = parties.id;
-
-SELECT candidates.*, parties.name AS party_name
-FROM candidates
-LEFT JOIN parties ON candidates.party_id = parties.id;
